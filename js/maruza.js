3c2f53e6787606cb7258c45808bdfe326a1059f4
Nurbek Normatov <nurbeknormatov729@gmail.com>
2023-01-13 13:50:35 +0500

    lets go

---
+++ js/maruza.js
@@ -0,0 +1,14 @@
+const disabled = document.querySelector("#maruzalar-1");
+console.log(disabled);
+const option =document.querySelector("#disabled");
+console.log(option);
+const disabledtwo = document.querySelector("#maruzalar-2");
+console.log(disabledtwo);
+const optiontwo = document.querySelector("#disabled-2");
+console.log(optiontwo);
+
+
+
+
+
+
